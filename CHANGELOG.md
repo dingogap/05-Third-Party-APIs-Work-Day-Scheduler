fb74110131ddd761b49eb4a47a4fd1672d502a71
Peter Medbury <peter@Deep-Thought.local>
2023-06-24 11:54:00 +1000

    Added code to write to & read from local storage and update the Work Day Planner

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,7 +14,8 @@
 1. Created folder structure to hold assets
 2. Created CHANGELOG.md
 3. Added code to add day to header
-4. Added code to build the Daily Planner
+4. Added code to build the Work Day Planner
 5. Added time displays to the hourly blocks
 6. Added Interval Timer to update hour blocks every hour
 7. Added Event Listener to detect Save Button clicks and get the data to be saved for the event
+8. Added code to write to & read from local storage and update the Work Day Planner
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,135 +1,72 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
+// Global variables
 var pastClass = "past";
 var presentClass = "present";
 var futureClass = "future";
 var dayStart = 9;
-var dayEnd = 20;
+var dayEnd = 17;
+var planner;
+var schedule = "schedule";
+
 $(function () {
+
+  // Read planner data from local storage if it exists
+  planner = readPlanner();
+  if (planner === null) {
+    // If planner data doesn't exist create & populate planner data object
+    planner = {}
+    for (var i = dayStart; i < dayEnd + 1; i++) {
+      planner[i] = "";
+    }
+  }
+
   //Build the Daily Planner
 
   // Get the date
   var dateAsString = dayjs().format("YYYY-MM-DD");
-  console.log(dayjs().format("D"));
-
-  // build the daily planner, checking the time to assign the class to show past, present & future
+  // Build each Time Block
   for (var i = dayEnd; i > dayStart - 1; i--) {
-    // build the id for the hourly blocks
+
+    // Create the Time Block
     var hourId = "hour-" + i.toString();
+    $(".container-lg").prepend("<div id='" + hourId + "' class='row time-block'></div>");
 
-    $(".container-lg").prepend(
-      "<div id='" +
-      hourId +"' class='row time-block'></div>"
-
-    );
-    $("#" + hourId).append(
-      "<div class='col-2 col-md-1 hour text-center py-3'></div>"
-    );
-    // hour as a string
+    $("#" + hourId).append("<div class='col-2 col-md-1 hour text-center py-3'></div>");
     hourAsString = i.toString() + ":00";
     // show the hour in 12 hour format AM/PM
-    $("#" + hourId + " div").text(
-      dayjs(dateAsString + " " + hourAsString).format("hA")
-    );
-    $("#" + hourId).append(
-      "<textarea class='col-8 col-md-10 description' rows='3'></textarea>"
-    );
-    $("#" + hourId).append(
-      "<button class='btn saveBtn col-2 col-md-1' aria-label='save'>"
-    );
+    $("#" + hourId + " div").text(dayjs(dateAsString + " " + hourAsString).format("hA"));
+
+    $("#" + hourId).append("<textarea class='col-8 col-md-10 description' rows='3'></textarea>");
+    // Get Meeting for this Time Block
+    $("#" + hourId + " textarea").text(planner[i]);
+
+    // Create Save Button
+    $("#" + hourId).append("<button class='btn saveBtn col-2 col-md-1' aria-label='save'>");
   }
-  // Add the save icon to all buttons with the saveBtn class
+  // Add the Save Icon to all elements with the saveBtn class
   $(".saveBtn").prepend("<i class='fas fa-save' aria-hidden='true'></i>");
 
   // Check the time and apply hour block colours
   checkTime();
 
+  // Event listener for Save Button 
   $(".saveBtn").on("click", function () {
-    console.log($(this).siblings(".description").val());
-    console.log($(this).parent().attr('id'));
+    meetingTime = parseInt($(this).parent().attr("id").split("-")[1]);
+    meetingDescription = $(this).siblings(".description").val();
+    planner[meetingTime] = meetingDescription
+    // Save Planner Data to local storage
+    localStorage.setItem(schedule, JSON.stringify(planner));
+  });
 
-  })
-
+  // 
   setInterval(function () {
     // Checks each minute to see if its the start of a new hour
     // If so, call checkTime and update the hour block classes
     var currentMinute = parseInt(dayjs().minute());
     if (currentMinute === 0) {
-      checkTime()
+      checkTime();
     }
-  }, 60000
-  )
-
-  function checkTime() {
-    // Get the current time
-    // Check the hour block divs to see if they are for past, present or future
-    // Call checkHourClass to ensure the class is correct for the time
-    var currentHour = parseInt(dayjs().hour());
-    for (var i = dayStart; i < dayEnd + 1; i++) {
-      var hourId = "#hour-" + i.toString();
-      if (i < currentHour) {
-        hourClass = pastClass;
-        checkHourClass(hourId, hourClass);
-      } else if (i > currentHour) {
-        hourClass = futureClass;
-        checkHourClass(hourId, hourClass);
-      } else {
-        hourClass = presentClass;
-        checkHourClass(hourId, hourClass);
-      }
-    }
-  }
-
-  function checkHourClass(hourId, hourClass) {
-    // Update the colour class based on time - past, present or future
-    switch (hourClass) {
-      case pastClass:
-        if ($(hourId).hasClass(presentClass)) {
-          $(hourId).removeClass(presentClass);
-        } else {
-          $(hourId).removeClass(futureClass);
-        }
-        break;
-      case presentClass:
-        if ($(hourId).hasClass(pastClass)) {
-          $(hourId).removeClass(pastClass);
-        } else {
-          $(hourId).removeClass(futureClass);
-        }
-        break;
-      case futureClass:
-        if ($(hourId).hasClass(pastClass)) {
-          $(hourId).removeClass(pastClass);
-        } else {
-          $(hourId).removeClass(presentClass);
-        }
-        break;
-    }
-    if (!$(hourId).hasClass(hourClass)) {
-      $(hourId).addClass(hourClass);
-    }
-  }
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  //Get todays date & format it for the Scheduler Header
+  }, 60000);
 
   // Get todays date
   var todaysDate = dayjs();
@@ -165,4 +102,59 @@ function getOrdinal(noToOrdinal) {
       ordinalSuffix = "th";
   }
   return ordinalSuffix;
+}
+
+function checkHourClass(hourId, hourClass) {
+  // Update the colour class based on time - past, present or future
+  switch (hourClass) {
+    case pastClass:
+      if ($(hourId).hasClass(presentClass)) {
+        $(hourId).removeClass(presentClass);
+      } else {
+        $(hourId).removeClass(futureClass);
+      }
+      break;
+    case presentClass:
+      if ($(hourId).hasClass(pastClass)) {
+        $(hourId).removeClass(pastClass);
+      } else {
+        $(hourId).removeClass(futureClass);
+      }
+      break;
+    case futureClass:
+      if ($(hourId).hasClass(pastClass)) {
+        $(hourId).removeClass(pastClass);
+      } else {
+        $(hourId).removeClass(presentClass);
+      }
+      break;
+  }
+  if (!$(hourId).hasClass(hourClass)) {
+    $(hourId).addClass(hourClass);
+  }
+}
+
+function readPlanner() {
+  // Read Planner Data from local storage
+  return JSON.parse(localStorage.getItem(schedule));
+}
+
+function checkTime() {
+  // Get the current time
+  // Check the hour block divs to see if they are for past, present or future
+  // Call checkHourClass to ensure the class is correct for the time
+  var currentHour = parseInt(dayjs().hour());
+  for (var i = dayStart; i < dayEnd + 1; i++) {
+    var hourId = "#hour-" + i.toString();
+    if (i < currentHour) {
+      hourClass = pastClass;
+      checkHourClass(hourId, hourClass);
+    } else if (i > currentHour) {
+      hourClass = futureClass;
+      checkHourClass(hourId, hourClass);
+    } else {
+      hourClass = presentClass;
+      checkHourClass(hourId, hourClass);
+    }
+  }
 }
